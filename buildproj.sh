53693a92a0cd6dea03f13baa27414538ce11f7d3
Pinaki <appugupta007@gmail.com>
2023-06-07 14:17:48 +0530

    treat as unspecified commit

--- buildproj.sh
+++ buildproj.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmake -S . -B build -D CMAKE_BUILD_TYPE=Release && \
+cmake -S . -B build -D CMAKE_BUILD_TYPE=Debug && \
 cmake --build build && \
 cd build && \
 chmod +x installscriptrunner && \
