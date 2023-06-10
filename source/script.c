c949f419eb8a563666e845e93d08ef2f24222992
Pinaki <appugupta007@gmail.com>
2023-06-10 13:27:52 +0530

    the command to intall apps will be read from a file instead of reading from a predefined macro. fn install_command()

--- source/script.c
+++ source/script.c
@@ -9,12 +9,12 @@
 #define MAXLINELEN 2048
 #define MAX_STRING_LENGTH_4_SPECIAL 100
 #define SUDOCOMMAND  "sudo"
-#define  INSTALLCOMMAND  "install"
 #define strndup  sf_strndup
 
 int readLineFromFile(FILE *file, int *totalLines, char ***lineContents);
 void copyStringWithoutPrefix(const char *input, char *output, const char *prefix);
 void package_manager(char *package_manager_name);
+void install_command(char *command_2_install_apps);
 int renewsys(void);
 int package_installer(void);
 
@@ -74,7 +74,7 @@ void copyStringWithoutPrefix(const char *input, char *output, const char *prefix
   strcpy(output, input);
 }
 
-void package_manager(char *package_manager_name) {
+void package_manager(char *package_manager_name) { // apt, yum, dnf, apx etc.
   int totalLines = 0;
   char **lineContents = NULL;
   FILE  *fp;      /* input-file pointer */
@@ -108,6 +108,42 @@ void package_manager(char *package_manager_name) {
   free(lineContents);
   lineContents = NULL;
   (void)fclose(fp); // Close the file
+}
+
+void install_command(char *command_2_install_apps) { // install, -S etc.
+  int totalLines = 0;
+  char **lineContents = NULL;
+  FILE  *file;      /* input-file pointer */
+  char  *file_install_command = "../.config/scriptrunner/installcommand.txt";      /* input-file name */ /* use extension within double quotes */
+  file  = fopen(file_install_command, "r");
+
+  if(file == NULL) {
+    (void)fprintf(stderr, "\ncouldn't open file '%s'; %s\n", file_install_command,  strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  // Call the readLineFromFile function to read the contents of the file
+  int result = readLineFromFile(file, &totalLines, &lineContents);
+
+  if(result != 0) {
+    (void)fprintf(stderr, "\nError reading file. '%s'; %s\n", file_install_command,  strerror(errno));
+    (void)fclose(file);
+  }
+
+  /* copy the contents of the line no. 0 to the variable command_2_install_apps
+    and pass it to command_2_install_apps */
+  if(lineContents[0] != NULL) {
+    strncpy(command_2_install_apps, *(lineContents + 0), MAXLINELEN);
+  }
+
+  // Free the allocated memory for lineContents
+  for(int i = 0; i < totalLines; i++) {
+    free(lineContents[i]);
+  }
+
+  free(lineContents);
+  lineContents = NULL;
+  (void)fclose(file); // Close the file
 }
 
 int renewsys(void) {
@@ -149,11 +185,11 @@ int renewsys(void) {
   return 0;
 }
 
-int package_installer(void) {
+int package_installer(void) { // app installer
   char package_manager_name[MAXLINELEN] = "";
   package_manager(package_manager_name);
   (void)printf("manager: %s\n", package_manager_name);
-  FILE *file = fopen("../.config/scriptrunner/install.txt", "r");
+  FILE *file = fopen("../.config/scriptrunner/apps.txt", "r");
 
   if(file == NULL) {
     (void)printf("Failed to open the file.\n");
@@ -198,17 +234,21 @@ int package_installer(void) {
     }
 
     else {
-      char tmpstr2[] = "";
-      char *the_package_manager = tmpstr2;
+      char tmpstr2[MAX_STRING_LENGTH_4_SPECIAL] = "";
+      char tmpstr3[MAX_STRING_LENGTH_4_SPECIAL] = "";
+      char *the_package_manager = tmpstr2; // apt, yum etc.
+      char *the_command_2_install_packages = tmpstr3; // install, -S etc.
       char totalcommandtopass[MAXLINELEN] = "";
       package_manager(the_package_manager);
-      /*(void)printf("The package manager found in the fn package installer: %s\n", the_package_manager);*/
+      install_command(the_command_2_install_packages);
+      /*(void)printf("The package manager found in the fn package_manager: %s\n", the_package_manager);*/
+      /*(void)printf("The install command found in the fn install_command: %s\n", the_command_2_install_packages);*/
       /* concatenate SUDOCOMMAND, the_package_manager, INSTALLCOMMAND, and the contents of the line */
       sf_strncat(totalcommandtopass, SUDOCOMMAND, MAXLINELEN);
       sf_strncat(totalcommandtopass, " ", MAXLINELEN);
       sf_strncat(totalcommandtopass, the_package_manager, MAXLINELEN);
       sf_strncat(totalcommandtopass, " ", MAXLINELEN);
-      sf_strncat(totalcommandtopass, INSTALLCOMMAND, MAXLINELEN);
+      sf_strncat(totalcommandtopass, the_command_2_install_packages, MAXLINELEN);
       sf_strncat(totalcommandtopass, " ", MAXLINELEN);
       sf_strncat(totalcommandtopass, lineContents[lineNumber - 1], MAXLINELEN);
       (void)printf("Line %d: %s", lineNumber, totalcommandtopass);
@@ -227,7 +267,6 @@ int package_installer(void) {
   (void)fclose(file); // Close the file
   return 0;
 }
-
 
 int main() {
   FILE *file01 = fopen("../example.txt", "r");
@@ -272,6 +311,9 @@ int main() {
   char package_manager_name[MAXLINELEN] = "";
   package_manager(package_manager_name);
   printf("Your package manager is: %s\n", package_manager_name);
+  char installcommandname[MAXLINELEN] = "";
+  install_command(installcommandname);
+  printf("Your install command is: %s\n", installcommandname);
   (void)renewsys();
   (void)package_installer();
   return 0;
