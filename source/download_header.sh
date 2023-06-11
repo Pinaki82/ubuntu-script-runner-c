88d40615e383e6b33f5b5577564e20cfeaa7f153
Pinaki <appugupta007@gmail.com>
2023-06-11 22:45:20 +0530

    added a header downloader script

---
+++ source/download_header.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+curl -fsSL https://raw.githubusercontent.com/Pinaki82/safe_c/main/include/sf_c.h --output sf_c.h
+
+# Or,
+
+# wget -O sf_c.h https://raw.githubusercontent.com/Pinaki82/safe_c/main/include/sf_c.h
